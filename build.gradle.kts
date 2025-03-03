c4f5057246fe21e0fbab11d90bf77f920c083711
Eduardo <lachdanan_3@hotmail.com>
2025-03-02 22:33:26 -0600

    Cleaning mess

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
